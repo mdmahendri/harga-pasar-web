770fc7c4ada3b22d0754568e0157fbc724f0ec8a
mahendri.md <mahendri.md@gmail.com>
2018-06-10 16:29:48 +0700

    fix typo on monitoring link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="/moinitoring">Monitoring</a>
+                    <a href="/monitor">Monitoring</a>
                     <a href="https://play.google.com/store/apps/details?id=com.mahendri.pasbeli">Android</a>
                     <a href="https://github.com/mdmahendri">GitHub</a>
                 </div>
